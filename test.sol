63af055a32b94db9c596c592adb1dd92abde7a63
2693287507 <2693287507@qq.com>
2025-08-08 16:15:57 +0800

    基础

--- test.sol
+++ test.sol
@@ -1,16 +1,45 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
+// struct 结构体
+// array 数组
+// mapping 映射
+
 contract HelloWorld {
     string sayStr = "Hello World";
-    function sayHello() public view returns(string memory) {
-        return addInfo(sayStr);
+    struct Info {
+        uint id;
+        string phrase;
+        address addr;
     }
+    Info[] infos;
+
+    mapping (uint256 _id => Info info) infoMapping;
+
+    function sayHello(uint256 _id) public view returns(string memory) {
+        if(infoMapping[_id].addr == address(0x0)) {
+            return addInfo(sayStr);
+        }
+        return addInfo(infoMapping[_id].phrase);
+        // for (uint256 i = 0; i < infos.length; i++) {
+        //     if(_id == infos[i].id) {
+        //         return addInfo(infos[i].phrase);
+        //     }
+        // }
+        // return addInfo(sayStr);
+    }
+
     function sayHelloLang(string memory params) public  {
         sayStr = params;
     }
 
     function addInfo(string memory params) internal pure returns (string memory) {
-        return string.concat(params, ' HHHHHH');
+        return string.concat(params, ' LJW');
+    }
+    
+    function setInfos(string memory message, uint256 _id) public {
+        Info memory info = Info(_id, message, msg.sender);
+        infoMapping[_id] = info;
+        infos.push(info);
     }
 }
